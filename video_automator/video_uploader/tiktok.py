6b7005160f9b7215dde6e1504aeaa6db9e542407
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2024-10-22 18:01:15 +0200

    Auth with cookies

--- video_automator/video_uploader/tiktok.py
+++ video_automator/video_uploader/tiktok.py
@@ -6,11 +6,10 @@ from .video_uploader import VideoUploader
 class TikTokUploader(VideoUploader):
     def __init__(self, cookies_path: str) -> None:
         self.cookies_path = cookies_path
-        self.auth = AuthBackend(cookies_path)
+        self.auth = AuthBackend(cookies=cookies_path)
 
     def upload(self, video_path: str) -> None:
         tiktok.upload_videos([video_path], self.auth)
 
     def upload_multiple(self, video_paths: list[str]) -> None:
         tiktok.upload_videos(video_paths, self.auth)
-
