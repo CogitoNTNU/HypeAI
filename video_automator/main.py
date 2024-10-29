51e7da41897985b4c0c574c2229457a3d2eff505
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2024-10-29 19:32:23 +0100

    TikTok uploader works

--- video_automator/main.py
+++ video_automator/main.py
@@ -4,7 +4,7 @@ from video_uploader import TikTokUploader
 def main():
     # DOES NOT WORK
     uploader = TikTokUploader("cookies.txt")
-    uploader.upload("video.mp4")
+    uploader.upload("./output/video.mp4")
 
 
 if __name__ == "__main__":
