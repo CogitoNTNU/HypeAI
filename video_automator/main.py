3faa33c98e1153e72c24cfb4217395083cf9444b
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2024-11-05 17:40:24 +0100

    Added test for automator

--- video_automator/main.py
+++ video_automator/main.py
@@ -1,10 +1,15 @@
 from video_uploader import TikTokUploader
+from video_automator import VideoAutomator
 
 
 def main():
     # DOES NOT WORK
     uploader = TikTokUploader("cookies.txt")
-    uploader.upload("./output/video.mp4")
+
+    automator = VideoAutomator()
+    automator.add_video_uploader(uploader)
+
+    # uploader.upload("./output/video.mp4")
 
 
 if __name__ == "__main__":
